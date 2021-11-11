a37bcb7403817510fbd99704b281328dbda5e030
Shaparenko Nikita <shaparenka_3@mail.ru>
2021-11-11 21:05:52 +0300

    MyReduce with tests

--- task1.js
+++ task1.js
@@ -1,27 +1,26 @@
 Array.prototype.myForEach = function (callback) {
-    for (let i = 0 ; i < this.length; i++){
-        callback (this[i], i, this)
-    }
- }
- 
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
 Array.prototype.myReduce = function (callback, initialValues) {
-    
-    let accamulator = ( initialValues === undefined ) ? this[0] : initialValues
-    const start = ( initialValues === undefined ) ? 1 : 0
+  let accamulator = initialValues === undefined ? this[0] : initialValues;
+  const start = initialValues === undefined ? 1 : 0;
 
-    for  (let i = start; i < this.length; i++) {
-       accamulator = callback (accamulator, this[i], index, this) 
-    }
-    return accamulator;
-}
+  for (let i = start; i < this.length; i++) {
+    accamulator = callback(accamulator, this[i], index, this);
+  }
+  return accamulator;
+};
 
- const arr = ["a", "b", "c"]
+const arr = ["a", "b", "c"];
 
- arr.myReduce(() => {
-     console.log();
- })
- 
- arr.myForEach((item, index, array) => {
-     console.log(item, ' ' ,index , ' ' )
- }) 
+arr.myForEach((item, index, array) => {
+  console.log(item, " ", index, " ");
+});
 
+const numArr = [1, 2, 3, 4, 5, 6];
+
+console.log(numArr.myReduce((sum, val) => sum + val)); //without initial value
+console.log(numArr.myReduce((sum, val) => sum + val, 100)); //with inital value
