4c146d5dceca7039fb9eb7ba6d756cf030f82282
Shaparenko Nikita <shaparenka_3@mail.ru>
2021-11-11 21:26:50 +0300

    Final

--- task1.js
+++ task1.js
@@ -1,15 +1,27 @@
 Array.prototype.myForEach = function (callback) {
-    for (let i = 0 ; i < this.length; i++){
-        callback (this[i], i, this)
-    }
- }
- 
-Array.prototype.myFilter = function (callback) {
-    let filteredArray = []
-}
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
 
- const arr = ["a", "b", "c"]
- 
- arr.myForEach((item, index, array) => {
-     console.log(item, ' ' ,index , ' ' )
- })
+Array.prototype.myFilter = function (callback) {
+  let filteredArray = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      filteredArray.push(his[i]);
+    }
+  }
+
+  return filteredArray;
+};
+
+const dataForFilter = [1, 2, 3, 4, 5];
+const arr = ["a", "b", "c"];
+
+const filterData = dataForFilter.myFilter((el) => {
+  if (el > 2) return el;
+});
+arr.myForEach((item, index, array) => {
+  console.log(item, " ", index, " ");
+});
