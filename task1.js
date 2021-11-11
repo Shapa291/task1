5ab804bb1f50526163d7c26001c2dbc8d159ccba
Shaparenko Nikita <shaparenka_3@mail.ru>
2021-11-11 14:20:43 +0300

    myReduce

--- task1.js
+++ task1.js
@@ -4,8 +4,24 @@ Array.prototype.myForEach = function (callback) {
     }
  }
  
+Array.prototype.myReduce = function (callback, initialValues) {
+    
+    let accamulator = ( initialValues === undefined ) ? this[0] : initialValues
+    const start = ( initialValues === undefined ) ? 1 : 0
+
+    for  (let i = start; i < this.length; i++) {
+       accamulator = callback (accamulator, this[i]) 
+    }
+    return accamulator;
+}
+
  const arr = ["a", "b", "c"]
+
+ arr.myReduce(() => {
+     console.log();
+ })
  
  arr.myForEach((item, index, array) => {
      console.log(item, ' ' ,index , ' ' )
- })
+ }) 
+
