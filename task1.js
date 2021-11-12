347d651a44d51d6d4db587b83190c9c5ada336eb
Shaparenko Nikita <shaparenka_3@mail.ru>
2021-11-12 12:47:51 +0300

    Add mySome with tests

--- task1.js
+++ task1.js
@@ -58,3 +58,26 @@ arr.myForEach((item, index, array) => {
 
 console.log(numArr.myReduce((sum, val) => sum + val)); //without initial value
 console.log(numArr.myReduce((sum, val) => sum + val, 100)); //with inital value
+
+Array.prototype.mySome = function (callback) {  
+    let booleanResult = false
+    for (let i = 0; i < this.length; i++){
+      if (callback(this[i])) booleanResult = true
+      if (callback(i))
+      if (callback(this)) booleanResult = true
+    }
+    return booleanResult;
+}
+
+const dataForTestSOME = [2, 5, 11, 1, 4]
+
+const checkResultArraySOME = (item, index, array) => {
+  return array.length > 1
+}
+
+const checkResultItemSOME = (item, index, array) => {
+  return item > 2
+}
+
+console.log(dataForTestSOME.some(checkResultArraySOME))
+console.log(dataForTestSOME.some(checkResultItemSOME))
